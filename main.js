ce5615e10d2c757651140b174782f038b4294b3a
delairs <delair27rs@gmail.com>
2023-11-06 09:25:48 +0700

    Perbaikan

--- main.js
+++ main.js
@@ -8,16 +8,20 @@ fetch("https://pokeapi.co/api/v2/pokemon?limit=20&offset=0")
     let cards = "";
     data.forEach((m) => {
       cards += `
-      <div class="col-xl-3 col-md-6 mb-4">
-        <div class="card border-primary mb-3" style="max-width: 18rem">
-          <div class="card-body text-primary text-center">
-            <p class="card-text text-dark">
-              ${m.name}
-            </p>
-            <a href="${m.url}" class="btn btn-primary">Button</a>
-          </div>
+      
+      <div class="col-xl-3 col-md-6 my-4">
+      <div class="card border-primary  shadow bg-body rounded" style="max-width: 18rem">
+        <div class="card-body text-primary text-center">
+          <p class="card-text text-dark">
+            ${m.name}
+          </p>
+          <a href="${m.url}" class="btn btn-primary">Button</a>
         </div>
-      </div>`;
+      </div>
+
+      
+      </div>
+      `;
     });
     const pokemonContainer = document.querySelector("#card");
     pokemonContainer.innerHTML = cards;
